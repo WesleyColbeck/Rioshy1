fca27b42cff812b20d07346e8400ca5a511f4e0d
Gosth <80866843+WesleyColbeck@users.noreply.github.com>
2026-03-01 17:53:33 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,9 +3,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO",
-  projectId: "SEU_PROJECT_ID",
+  apiKey: "AIzaSyDAV06sRQLf3zeXh-C77-49BRoMtAiEiKw",
+  authDomain: "rioshycardapio.firebaseapp.com",
+  projectId: "rioshycardapio",
+  storageBucket: "rioshycardapio.firebasestorage.app",
+  messagingSenderId: "673349899022",
+  appId: "1:673349899022:web:9c6e3a0d5d24bf1ab37d5f",
+  measurementId: "G-B53FRXNELP",
 };
 
 const app = initializeApp(firebaseConfig);
